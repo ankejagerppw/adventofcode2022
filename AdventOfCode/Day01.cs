01a5df2008bd44973b84f84f304ec8f51dd55a03
Anke Jäger <anke_jager@peopleware.be>
2022-12-02 11:41:55 +0100

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,63 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File
+            .ReadLines(InputFilePath)
+            .ToArray();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        int maxCalories = 0;
+        int sumCalories = 0;
+        foreach (string line in _input)
+        {
+            int nbrCalories;
+            if (int.TryParse(line, out nbrCalories))
+            {
+                sumCalories += nbrCalories;
+            }
+            else
+            {
+                if (sumCalories > maxCalories)
+                {
+                    maxCalories = sumCalories;
+                }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+                sumCalories = 0;
+            }
+        }
+
+        return new ValueTask<string>($"Max nbr calories: {maxCalories}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        List<int> calories = new List<int>();
+
+        List<int> caloriesPerElf = new List<int>();
+        foreach (string line in _input)
+        {
+            if (int.TryParse(line, out int nbrCalories))
+            {
+                caloriesPerElf.Add(nbrCalories);
+            }
+            else
+            {
+                calories.Add(caloriesPerElf.Sum());
+                caloriesPerElf.Clear();
+            }
+        }
+
+        int sumTopCalories = calories
+            .OrderByDescending(i => i)
+            .Take(3)
+            .Sum();
+
+        return new($"Sum of top 3 calories: {sumTopCalories}");
+    }
 }
